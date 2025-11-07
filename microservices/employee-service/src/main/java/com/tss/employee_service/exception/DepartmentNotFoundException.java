cd20d3006116940a9468026c04ca2467f79b4d2e
Dhyey Shiyal <147261942+sdhyey26@users.noreply.github.com>
2025-11-07 11:51:56 +0530

    Delete microservices directory

--- microservices/employee-service/src/main/java/com/tss/employee_service/exception/DepartmentNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.tss.employee_service.exception;
-
-public class DepartmentNotFoundException extends RuntimeException {
-    public DepartmentNotFoundException(String message) {
-        super(message);
-    }
-}
